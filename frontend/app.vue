64fe38dc9223ab7c45c1bbf10852a0918cd48bb8
Alec <alec_z17@web.de>
2026-01-27 10:23:48 +0100

    added frontend

---
+++ frontend/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <header v-if="navigation">
+    <NuxtLink v-for="{ link, title } in navigation" :key="link" :to="link">
+      {{ title }}
+    </NuxtLink>
+  </header>
+  <NuxtPage />
+</template>
+<script setup lang="ts">
+const { initialData } = useT3Api()
+const navigation = computed(
+  () => initialData.value?.navigation?.[0]?.children ?? []
+)
+</script>
+<style scoped>
+header a {
+  display: inline-block;
+  margin-right: 10px;
+}
+</style>
